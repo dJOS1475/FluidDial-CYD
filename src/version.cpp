99801c4295c3913cd8fc712ca702965e2fed359f
dJOS1475 <derekjosborn@gmail.com>
2026-03-31 09:32:45 +1030

    v1.4.0 - Alarm descriptions, motion smoothness, jog speed dial, feed/spindle override dial

--- src/version.cpp
+++ src/version.cpp
@@ -1,2 +1,2 @@
-const char* git_info     = "v1.4.0main-dirty";
+const char* git_info     = "v1.3.0main-33c951a-dirty";
 const char* git_url      = "https://dJOS1475@github.com/dJOS1475/FluidDial-CYD.git";
